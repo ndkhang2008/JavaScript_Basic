fbe2eed328bd38dc15041e352734ff08ee07c9f2
ndkhang2008 <bichpham.1970t@gmail.com>
2025-06-11 21:07:48 +0700

    learn about terminology

---
+++ code-css-js/01.js
@@ -0,0 +1,17 @@
+var fullName = ('Nguyễn Đăng Khang');
+var age = (17);
+
+// alert(fullName);
+// alert(age); 
+// confirm('Bạn đả hoàn thành khoá học chưa')
+// prompt('nhập độ tuổi của bạn')
+// setTimeout(function() {
+//     alert(fullName)
+// }, 1000)
+setInterval(function() {
+    console.log(fullName + Math.random())
+},2000)
+
+console.log(123)
+console.warn('day la 1 dong log')
+console.error(fullName)

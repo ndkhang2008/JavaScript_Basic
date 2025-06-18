1f7ccd63e2a903a1114281435b8babae9d5cc918
ndkhang2008 <bichpham.1970t@gmail.com>
2025-06-18 19:06:28 +0700

    string format

---
+++ code-css-js/05.js
@@ -0,0 +1,18 @@
+var myString = '   Hello new day         '
+
+console.log(myString.length) //số kí tự
+
+console.log(myString.indexOf('new')) //vị trí của kí tự
+
+console.log(myString.slice(6, 10)) //cắt nội dung
+
+// console.log(myString.replace('new day', 'you')) //ghi đè
+
+// console.log(myString.toUpperCase()) //viết hoa all kí tự
+
+// console.log(myString.toLowerCase()) //viết thwg all kí tự
+
+// console.log(myString.trim()) //loại bỏ khoảng trắng thừa
+
+var languages = 'JavaScript, PHP, Ruby'
+console.log(languages.split(', '))

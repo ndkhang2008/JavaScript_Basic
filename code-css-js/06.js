28602684a55684a9ccc210c5ae0d6dda8c45ae83
ndkhang2008 <bichpham.1970t@gmail.com>
2025-06-21 19:50:01 +0700

    complete array

---
+++ code-css-js/06.js
@@ -0,0 +1,26 @@
+var languages = [
+    'Javascript',
+    'PHP',
+    'Ruby'
+]
+
+var languages2 = [
+    'Dart',
+    'Java',
+]
+
+// console.log(Array.isArray(languages))
+// console.log(languages[2])
+
+// console.log(languages.join(', '))
+// console.log(languages.pop())
+// console.log(languages.push('Dart', 'Java'))
+// console.log(languages.shift())
+// console.log(languages.unshift('Javascript'))
+// languages.splice(1, 1)
+
+// console.log(languages.splice(1, 1))
+// console.log(languages.splice(1, 0, 'Dart'))
+console.log(languages.concat(languages2))
+console.log(languages.slice(1, 3))
+console.log(languages) 
